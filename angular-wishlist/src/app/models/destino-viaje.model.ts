587cb51744c88b2d9adb91411dd8020f74593391
Angel-Znoker <angel96esti@comunidad.unam.mx>
2020-11-16 19:04:08 -0600

    prototipado y vistas

--- angular-wishlist/src/app/models/destino-viaje.model.ts
+++ angular-wishlist/src/app/models/destino-viaje.model.ts
@@ -1,9 +1,13 @@
 export class DestinoViaje {
   private selected: boolean;
+  public servicios: string[];
 
   constructor(
     public nombre: string,
-    public url: string) { }
+    public url: string
+  ) {
+    this.servicios = ['piscina', 'desayuno'];
+  }
 
   isSelected(): boolean {
     return this.selected;
